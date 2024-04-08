291bdc38f8ec822ef51010867731637d8827de21
Guoqi Sun <sun412926@gmail.com>
2024-04-08 12:20:01 +0800

    feat: update lp docs

---
+++ .vitepress/config.mjs
@@ -0,0 +1,118 @@
+import { defineConfig } from "vitepress";
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  base: "/lp/",
+  ignoreDeadLinks: true,
+  title: "芦笋录屏",
+  description:
+    "芦笋录屏，极简视频录制工具。同时录制人像和屏幕，美颜、虚拟头像等让视频信息更生动；支持视频一键分享，在线剪辑等。",
+  lang: "zh-Hans",
+  head: [
+    ["link", { rel: "icon", href: "/lp/favicon.ico" }],
+    [
+      "script",
+      {},
+      `var _hmt = _hmt || [];
+      (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?592ba55993f3df6afcc69f8088276562";
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(hm, s);
+      })();`,
+    ],
+  ],
+  markdown: {
+    image: {
+      // 默认禁用图片懒加载
+      lazyLoading: true,
+    },
+  },
+  themeConfig: {
+    search: {
+      provider: "local",
+      options: {
+        placeholder: "搜索文档",
+        translations: {
+          button: {
+            buttonText: "搜索文档",
+            buttonAriaLabel: "搜索文档",
+          },
+          modal: {
+            searchBox: {
+              resetButtonTitle: "清除查询条件",
+              resetButtonAriaLabel: "清除查询条件",
+              cancelButtonText: "取消",
+              cancelButtonAriaLabel: "取消",
+            },
+            startScreen: {
+              recentSearchesTitle: "搜索历史",
+              noRecentSearchesText: "没有搜索历史",
+              saveRecentSearchButtonTitle: "保存至搜索历史",
+              removeRecentSearchButtonTitle: "从搜索历史中移除",
+              favoriteSearchesTitle: "收藏",
+              removeFavoriteSearchButtonTitle: "从收藏中移除",
+            },
+            errorScreen: {
+              titleText: "无法获取结果",
+              helpText: "你可能需要检查你的网络连接",
+            },
+            footer: {
+              selectText: "选择",
+              navigateText: "切换",
+              closeText: "关闭",
+              searchByText: "搜索提供者",
+            },
+            noResultsScreen: {
+              noResultsText: "无法找到相关结果",
+              suggestedQueryText: "你可以尝试查询",
+              reportMissingResultsText: "你认为该查询应该有结果？",
+              reportMissingResultsLinkText: "点击反馈",
+            },
+          },
+        },
+      },
+    },
+    // https://vitepress.dev/reference/default-theme-config
+    docFooter: {
+      prev: "上一页",
+      next: "下一页",
+    },
+
+    outline: {
+      level: [2, 3],
+      label: "页面导航",
+    },
+
+    lastUpdated: {
+      text: "最后更新于",
+      formatOptions: {
+        dateStyle: "full",
+        timeStyle: "medium",
+      },
+    },
+    langMenuLabel: "多语言",
+    returnToTopLabel: "回到顶部",
+    sidebarMenuLabel: "菜单",
+    darkModeSwitchLabel: "切换主题",
+    lightModeSwitchTitle: "切换到浅色模式",
+    darkModeSwitchTitle: "切换到深色模式",
+    nav: [
+      { text: "芦笋提词器", link: "https://tcq.lusun.com" },
+      { text: "关于我们", link: "https://lusun.com/about" },
+    ],
+    sidebar: [
+      {
+        text: "基础教程",
+        collapsed: false,
+        items: [
+          { text: "电脑端芦笋录屏攻略", link: "/basic/pc" },
+          { text: "移动端芦笋录屏攻略", link: "/basic/phone" },
+        ],
+      },
+      { text: "进阶教程", link: "/advanced" },
+      { text: "常见问题", link: "/faq" },
+      { text: "联系我们", link: "/contact/" },
+    ],
+  },
+});
---
+++ basic/pc.md
@@ -0,0 +1,70 @@
+---
+description: 这篇介绍了如何使用电脑端使用芦笋录屏，以及芦笋录屏电脑端的下载方式
+---
+
+# 电脑端芦笋录屏攻略
+
+## 整体介绍—如何使用电脑录制
+
+<iframe src="https://lusun.com/embed/?id=nrLsJuK8lZJ" width="100%" height="500px" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"> </iframe>
+
+## 分步教程
+
+### 芦笋录屏如何下载
+
+你可以通过以下两种方式，下载使用芦笋录屏的电脑客户端
+
+1. 百度搜索『芦笋官网』
+2. 浏览器访问网址 lusun.com
+
+<iframe src="https://lusun.com/embed/?id=Zom2AUGEPbx" width="100%" height="500px" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"></iframe>
+
+### 录制详情介绍
+
+<br>
+
+#### 三种录制模式
+
+芦笋录屏提供了【仅人像】、【人像+屏幕】、【仅屏幕】 三种录制模式，下面的简短视频演示了三种模式对应的效果，帮助你快速了解录制的主功能
+
+<iframe src="https://lusun.com/embed/?id=F7jdJEYKy67" width="100%" height="500px" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"></iframe>
+<br>
+
+#### 四种人像模式
+
+芦笋录屏提供了 4 种人像模式—【圆形人像、方形头像、虚拟背景、智能抠像】，下面的图片展示了 4 种模式对应的效果预览图
+
+<img src="../.gitbook/assets/luping4renxiang.png" alt=""><p>芦笋录屏录制人像的 4 种样式</p>
+
+<iframe src="https://lusun.com/embed/?id=HHZzCyivDIv" width="100%" height="500px" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"></iframe>
+
+<br>
+
+#### 绿幕抠像功能
+
+你可以在芦笋录屏使用绿幕抠像功能，实体绿幕会让抠像的效果更精致。可参考以下视频了解详情
+
+<iframe src="https://lusun.com/embed/?id=Ikd1Rw6476i" width="100%" height="500px" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"></iframe>
+
+<br>
+
+#### 其它设置项
+
+你可以通过打开芦笋录屏的客户端，点击右上角的头像，里面支持 「设置」 更多，可以设置摄像头、宽高比例、开/关美颜、镜头翻转、鼠标高亮等。
+
+![](https://help.lusun.com/content/images/2022/12/------2.png)
+
+#### 录制中的操作介绍 <a href="#f0-9f-91-89-e5-bd-95-e5-88-b6-e4-b8-a-d-e7-9a-84-e6-93-8d-e4-bd-9c" id="f0-9f-91-89-e5-bd-95-e5-88-b6-e4-b8-a-d-e7-9a-84-e6-93-8d-e4-bd-9c"></a>
+
+你可以通过观看以下视频快速了解录制过程中，如何查看录制时间、暂停 / 继续录制、取消录制、开关音像设置、使用画笔功能等能力。
+
+<iframe src="https://lusun.com/embed/?id=etvxS4eOK06" width="100%" height="500px" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"></iframe>
+
+### 录制后视频存储
+
+_演示保存本地/上传到云空间_
+
+### 云空间视频分享
+
+\
+_演示分享链接_
